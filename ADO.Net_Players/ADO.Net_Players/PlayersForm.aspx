ab23f799525212ce4edb10e36142c259ede168eb
varathan <varathansuyambu95@gmail.com>
2018-08-14 23:13:16 +0530

    ADO.NET CURD project added

---
+++ ADO.Net_Players/ADO.Net_Players/PlayersForm.aspx
@@ -0,0 +1,121 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PlayersForm.aspx.cs" Inherits="ADO.Net_Players.PlayersForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            height: 23px;
+        }
+        .auto-style3 {
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="btnCheck" runat="server" OnClick="btnCheck_Click" Text="Check" />
+        <asp:Label ID="lblCheck" runat="server"></asp:Label>
+    
+        <br />
+        <br />
+        <table class="auto-style1">
+            <tr>
+                <td>Name</td>
+                <td>
+                    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>DOB</td>
+                <td>
+                    <asp:TextBox ID="txtDOB" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Club</td>
+                <td>
+                    <asp:TextBox ID="txtClub" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Salary</td>
+                <td>
+                    <asp:TextBox ID="txtSalary" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3"></td>
+                <td class="auto-style3">
+                    <asp:Button ID="btnAddPlayer" runat="server" OnClick="btnAddPlayer_Click" Text="ADD" />
+                    <asp:Button ID="btnParaAdd" runat="server" OnClick="btnParaAdd_Click" Text="Parameterized Add" />
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:Label ID="lblResult" runat="server" ForeColor="Red"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+        <table class="auto-style1">
+            <tr>
+                <td>Name</td>
+                <td>
+                    <asp:DropDownList ID="ddlName" runat="server">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>Club</td>
+                <td>
+                    <asp:TextBox ID="txtUpdateClub" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Salary</td>
+                <td>
+                    <asp:TextBox ID="txtUpdateSalary" runat="server" ></asp:TextBox>
+                    </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Update" />
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:Label ID="lblUpdate" runat="server" ForeColor="Red"></asp:Label>
+                </td>
+            </tr>
+        </table>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">ToView Data Reader</td>
+                <td class="auto-style2">
+                    <asp:Button ID="btnReader" runat="server" OnClick="btnReader_Click" Text="Reader" />
+                </td>
+            </tr>
+            <tr>
+                <td>ToView Data Adapter</td>
+                <td>
+                    <asp:Button ID="btnAdapter" runat="server" OnClick="btnAdapter_Click" Text="Adapter" />
+                </td>
+            </tr>
+        </table>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ ADO.Net_Players/ADO.Net_Players/PlayersForm.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ADO.Net_Players
+{
+    public partial class PlayersForm : System.Web.UI.Page
+    {
+        string strconnection = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+
+        public object sqlDbType { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                SqlConnection connection = new SqlConnection(strconnection);
+                SqlCommand selectQuery = new SqlCommand("select * from players", connection);
+                //SqlDataAdapter adapter = new SqlDataAdapter(selectQuery);
+                //DataTable dt = new DataTable();
+                //adapter.Fill(dt);
+                //ddlName.DataSource = dt;
+                //ddlName.DataBind();
+                connection.Open();
+                SqlDataReader reader = selectQuery.ExecuteReader();
+                ddlName.DataSource = reader;
+                ddlName.DataTextField = "Name";
+                ddlName.DataValueField = "Name";
+                ddlName.DataBind();
+                connection.Close();
+            }
+        }
+
+        protected void btnCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(strconnection);
+                connection.Open();
+                lblCheck.Text = "Connection opened";
+                connection.Close();
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex);
+
+            }
+        }
+
+        protected void btnAddPlayer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ans = 0;
+                SqlConnection connection = new SqlConnection(strconnection);
+                SqlCommand addQuery = new SqlCommand("insert into players values('"+txtName.Text+"','"+Convert.ToDateTime(txtDOB.Text)+"','"+txtClub.Text+"',"+Convert.ToInt32(txtSalary.Text)+")", connection);
+                connection.Open();
+                 ans = addQuery.ExecuteNonQuery();
+                connection.Close();
+                if (ans >=1) { lblResult.Text = "Player Added"; }
+                else { lblResult.Text = "Player Not Added"; }
+            }
+            catch(Exception ex) { lblResult.Text = "Error"; }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ans = 0;
+                SqlConnection connection = new SqlConnection(strconnection);
+                SqlCommand updateQuery = new SqlCommand("update players set Club='" + txtUpdateClub.Text + "',Salary='" + Convert.ToInt32(txtUpdateSalary.Text) + "' where Name='" + ddlName.SelectedValue + "'", connection);
+                connection.Open();
+                ans = updateQuery.ExecuteNonQuery();
+                if (ans > 0) { lblUpdate.Text = "Player updated"; }
+                else { lblUpdate.Text = "Player Not updated"; }
+            }
+            catch (Exception ex) { lblUpdate.Text = "Error"; }
+        }
+
+        protected void btnReader_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(strconnection);
+            SqlCommand selectQuery = new SqlCommand("select * from players", connection);
+            connection.Open();
+            SqlDataReader reader = selectQuery.ExecuteReader();
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+            connection.Close();
+        }
+
+        protected void btnAdapter_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(strconnection);
+            SqlCommand selectQuery = new SqlCommand("select * from players", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(selectQuery);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void btnParaAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(strconnection);
+                SqlCommand paraAdd = new SqlCommand("insert into players values(@Name,@DOB,@Club,@Salary)", connection);
+                paraAdd.Parameters.AddWithValue("@Name", txtName.Text);
+                paraAdd.Parameters.AddWithValue("@DOB", Convert.ToDateTime(txtDOB.Text));
+                //paraAdd.Parameters.AddWithValue("@Club", txtClub.Text);
+                paraAdd.Parameters.Add("@Club", SqlDbType.Char);
+                paraAdd.Parameters["@Club"].Value = txtClub.Text;
+                paraAdd.Parameters.Add("@Salary",SqlDbType.Int);
+                paraAdd.Parameters["@Salary"].Value = txtSalary.Text;
+                connection.Open();
+                int ans = paraAdd.ExecuteNonQuery();
+                connection.Close();
+                if (ans >= 1) { lblResult.Text = "Player Added"; }
+                else { lblResult.Text = "Player Not Added"; }
+            }
+            catch (Exception ex) {
+                lblResult.Text =Convert.ToString(ex);
+            }
+        }
+    }
+}
